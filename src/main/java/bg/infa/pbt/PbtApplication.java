34b994d77e35d0ee1938b07443a1fbd1033f29ab
iRusev90 <ivan.rusev90@gmail.com>
2019-06-06 19:23:17 +0300

    initial commit after spring boot initializer

---
+++ src/main/java/bg/infa/pbt/PbtApplication.java
@@ -0,0 +1,13 @@
+package bg.infa.pbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/infa/pbt/PbtApplicationTests.java
@@ -0,0 +1,16 @@
+package bg.infa.pbt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PbtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
